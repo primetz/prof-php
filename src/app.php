2a9758e8a195d10b0218a8b13fde89d85ec56f4f
Stanislav Rodikov <stanislav.rodikov@yahoo.com>
2022-02-20 20:38:24 +0300

    lesson_1

--- src/app.php
+++ src/app.php
@@ -5,9 +5,19 @@ use Geekbrains\Person\Name;
 use Geekbrains\Person\Person;
 
 spl_autoload_register(function ($class) {
-    $file = sprintf('src/%s.php', str_replace('\\', DIRECTORY_SEPARATOR, $class));
+    $arr = explode('\\', $class);
+
+    $className = array_pop($arr);
+
+    $className = str_replace('_', DIRECTORY_SEPARATOR, $className);
+
+    $arr[] = $className;
+
+
+    $file = sprintf('src/%s.php', implode(DIRECTORY_SEPARATOR, $arr));
 
     if (file_exists($file)) {
+        var_dump($file);
         require $file;
     }
 });
@@ -24,4 +34,6 @@ $post = new Post(
 
 echo $post;
 
-echo '<br>';
+echo new \my\package_name\Class_Name('John');
+
+echo new \my\package\Class_Name('John');
